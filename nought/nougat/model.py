568f5dd7400cb5100741448bbdf2f8a95fd3885c
jhw <jhw15763365981@gmail.com>
2024-03-01 23:49:39 +0800

    2024.3.1 nougt/nougat/model.py明天继续，完成编写+分析+学习函数

--- nought/nougat/model.py
+++ nought/nougat/model.py
@@ -165,3 +165,166 @@ class SwinEncoder(nn.Module):
             img = img.rotate(img, angle=-90, expand=True)
         img = resize(img, min(self.input_size))
         img.thumbnail(self.input_size[1], self.input_size[0])
+        delta_width = self.input_size[1] - img.width
+        delta_height = self.input_size[0] - img.height
+        if random_padding:
+            pad_width = np.random.randint(low=0, high=delta_width + 1)
+            pad_height = np.random.randint(low=0, high=delta_height + 1)
+        else:
+            pad_width = delta_width // 2
+            pad_height = delta_height // 2
+        padding = (
+            pad_width,
+            pad_height,
+            delta_width - pad_width,
+            delta_height - pad_height,
+        )
+        return self.to_tensor(ImageOps.expand(img, padding))
+    
+class BartDecoder(nn.Module):
+    """
+    Decoder based on Multilingual BART
+    Set the initial weights and configuration with a pretrained multilingual BART model,
+    and modify the detailed configurations as a Nougat decoder
+
+    Args:
+        decoder_layer:
+            Number of layers of BARTDecoder
+        max_position_embeddings:
+            The maximum sequence length to be trained
+        name_or_path:
+            Name of a pretrained model name either registered in huggingface.co. or saved in local,
+            otherwise, `facebook/mbart-large-50` will be set (using `transformers`)
+"""
+    def __init__(
+            self,
+            decoder_layer: int,
+            max_position_embeddings: int,
+            hidden_dimention: int = 1024,
+            name_or_path: Union[str, bytes, os.PathLike] = None,
+    ):
+        super().__init__()
+        self.decoder_layer = decoder_layer
+        self.max_position_embeddings = max_position_embeddings
+        if not name_or_path:
+            tokenizer_file = Path(__file__).parent / "dataset" / "tokenizer.json"
+        else:
+            tokenizer_file = Path(name_or_path) / "tokenizer.json"
+        if not tokenizer_file.exists():
+            raise ValueError("Could not fine tokenizer file")
+        self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=str(tokenizer_file))
+        self.tokenizer.pad_token = "<pad>"
+        self.tokenizer.bos_token = "<s>"
+        self.tokenizer.eos_token = "</s>"
+        self.tokenizer.unk_token = "<unk>"
+
+        self.model = MBartForCausalLM(
+            config=MBartConfig(
+                is_decoder=True,
+                is_encoder_decoder=False,
+                add_cross_attention=True,
+                decoder_layers=self.decoder_layer,
+                max_position_embeddings=self.max_position_embeddings,
+                vocab_size=len(self.tokenizer),
+                scale_embedding=True,
+                add_final_layer_norm=True,
+                d_model=hidden_dimention,
+            )
+        )
+        self.model.config.is_encoder_decoder = True # to get cross-attention
+        self.model.model.decoder.embed_tokens.padding_idx = self.tokenizer.pad_token_id
+        self.model.prepare_inputs_for_generation = self.prepare_inputs_for_inference
+
+        if not name_or_path:
+            bart_state_dict = MBartForCausalLM.from_pretrained(
+                "facebook/mbart-large-50"
+            ).state_dict()
+            new_bart_state_dict = self.model.state_dict()
+            for x in new_bart_state_dict:
+                if (
+                    x.endswith("embed_positions.weight")
+                    and self.max_position_embeddings != 1024
+                ):
+                    new_bart_state_dict[x] = torch.nn.Parameter(
+                        self.resize_bart_abs_pos_emb(
+                            bart_state_dict[x],
+                            self.max_position_embeddings
+                            + 2,  # https://github.com/huggingface/transformers/blob/v4.11.3/src/transformers/models/mbart/modeling_mbart.py#L118-L119
+                        )
+                    )
+                elif x.endswith("embed_tokens.weight") or x.endswith("lm_head.weight"):
+                    new_bart_state_dict[x] = bart_state_dict[x][
+                        : len(self.tokenizer), :
+                    ]
+                else:
+                    new_bart_state_dict[x] = bart_state_dict[x]
+            self.model.load_state_dict(new_bart_state_dict)
+    
+    def add_special_tokens(self, list_of_tokens: List[str]):
+        """
+        Add special tokens to tokenizer and resize the token embeddings
+        """
+        newly_added_num = self.tokenizer.add_special_tokens(
+            {"additional_special_tokens": sorted(set(list_of_tokens))}
+        )
+        if newly_added_num > 0:
+            self.model.resize_token_embeddings(len(self.tokenizer))
+    
+    def prepare_inputs_for_inference(
+            self,
+            input_ids: torch.Tensor,
+            encoder_outputs: torch.Tensor,
+            past=None,
+            past_key_values=None,
+            use_cache: bool=None,
+            attention_mask: torch.Tensor=None,
+    ):
+        """
+        Args:
+            input_ids: (batch_size, sequence_length)
+
+        Returns:
+            input_ids: (batch_size, sequence_length)
+            attention_mask: (batch_size, sequence_length)
+            encoder_hidden_states: (batch_size, sequence_length, embedding_dim)
+        """
+        attention_mask = input_ids.ne(self.tokenizer.pad_token_id).long()
+        past = past or past_key_values
+        if past is not None:
+            input_ids = input_ids[:, -1:] 
+        outputs = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "past_key_values": past,
+            "use_cache": use_cache,
+            "encoder_hidden_states": encoder_outputs.last_hidden_state,
+        }
+        return outputs
+    
+    def forward(
+            self,
+            input_ids,
+            attention_mask: Optional[torch.Tensor] = None,
+            encoder_hidden_states: Optional[torch.Tensor] = None,
+            past_key_values: Optional[torch.Tensor] = None,
+            labels: Optional[torch.Tensor] = None,
+            use_cache: bool = None,
+            output_attentions: Optional[torch.Tensor] = None,
+            output_hidden_states: Optional[torch.Tensor] = None,
+            return_dict: bool = None
+    ):
+        return self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            labels=labels,
+            encoder_hidden_states=encoder_hidden_states,
+            past_key_values=past_key_values,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+
+
+
