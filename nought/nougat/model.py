a6a9587911a87d48ea6324eb219c70ddd03df4f2
jhw <jhw15763365981@gmail.com>
2024-03-02 20:47:44 +0800

    2024.3.2 完成model.py的代码，正在学习代码结构，涉及一些模型的理解，swintransform和bartdecode要去学一下

--- nought/nougat/model.py
+++ nought/nougat/model.py
@@ -325,6 +325,363 @@ class BartDecoder(nn.Module):
             return_dict=return_dict,
         )
 
+    @staticmethod
+    def resiz_bart_abs_pos_emb(
+        weight: torch.Tensor,
+        max_length: int,
+    ) -> torch.Tensor:
+        """
+        Resize position embeddings
+        Truncate if sequence length of MBart backbone is greater than given max_length,
+        else interpolate to max_length
+        """
+        if weight.shape[0] > max_length:
+            weight = weight[:max_length, ...]
+        else:
+            weight = (
+                F.interpolate(
+                    weight.permute(1, 0).unsqueeze(0),
+                    size=max_length,
+                    mode="linear",
+                    align_corners=False,
+                ).squeeze(0).permute(1, 0)
+            )
+        return weight
+
+class NougatConfig(PretrainedConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`NougatModel`]. It is used to
+    instantiate a Nougat model according to the specified arguments, defining the model architecture
+
+    Args:
+        input_size:
+            Input image size (canvas size) of Nougat.encoder, SwinTransformer in this codebase
+        align_long_axis:
+            Whether to rotate image if height is greater than width
+        window_size:
+            Window size of Nougat.encoder, SwinTransformer in this codebase
+        encoder_layer:
+            Depth of each Nougat.encoder Encoder layer, SwinTransformer in this codebase
+        decoder_layer:
+            Number of hidden layers in the Nougat.decoder, such as BART
+        max_position_embeddings
+            Trained max position embeddings in the Nougat decoder,
+            if not specified, it will have same value with max_length
+        max_length:
+            Max position embeddings(=maximum sequence length) you want to train
+        name_or_path:
+            Name of a pretrained model name either registered in huggingface.co. or saved in local
+    """
+    model_type = "nougat"
+
+    def __init__(
+            self,
+            input_size: List[int] = [896, 672],
+            align_long_axis: bool = False,
+            window_size: int = 7,
+            encoder_layer: List[int] = [2, 2, 14, 2],
+            decoder_layer: int=10,
+            max_position_embeddings: int = None,
+            max_length: int = 4096,
+            name_or_path: Union[str, bytes, os.Pathlike] = "",
+            patch_size: int = 4,
+            embed_dim: int = 128,
+            num_heads: List[int] = [4, 8, 16, 32],
+            hidden_dimension: int = 1024,
+            **kwargs,
+    ):
+        super().__init__()
+        self.input_size = input_size
+        self.align_long_axis = align_long_axis
+        self.window_size = window_size
+        self.encoder_layer = encoder_layer
+        self.decoder_layer = decoder_layer
+        self.max_position_embeddings = (
+            max_length if max_position_embeddings is None else max_position_embeddings
+        )
+        self.max_length = max_length
+        self.name_or_path = name_or_path
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.hidden_dimension = hidden_dimension
 
 
+class RunningVarTorch:
+    def __init__(self, L=15, norm=False):
+        self.values = None
+        self.L = L
+        self.norm = norm
+    
+    def push(self, x: torch.Tensor):
+        assert x.dim() == 1
+        if self.values is None:
+            self.values = x[:, None]
+        elif self.values.shape[1] < self.L:
+            self.values = torch.cat((self.values, x[:, None]), dim=1)
+        else:
+            self.values = torch.cat((self.values[:, 1:], x[:, None]), dim=1)
+    
+    def variance(self,):
+        if self.values is None:
+            return
+        if self.norm:
+            return torch.var(self.values, 1) / self.values.shape[1]
+        else:
+            return torch.var(self.values, 1)
+
+class StoppingCriteriaScores(StoppingCriteria):
+    def __init__(
+            self,
+            threshold: float = 0.015,
+            window_size: int = 200,
+    ):
+        super().__init__()
+        self.threshold = threshold
+        self.vars = RunningVarTorch(norm=True)
+        self.varvars = RunningVarTorch(L=window_size)
+        self.stop_inds = defaultdict(int)
+        self.stopped = defaultdict(bool)
+        self.size = 0
+        self.window_size = window_size
+
+    @torch.no_grad()
+    def __call__(
+        self,
+        input_ids: torch.LongTensor,
+        scores: torch.FloatTensor,
+    ):
+        last_scores = scores[-1]
+        self.vars.push(last_scores.max(1)[0].float().cpu())
+        self.varvars.push(self.vars.variance())
+        self.size += 1
+        if self.size < self.window_size:
+            return False
+    
+        varvars = self.varvars.variance()
+        for b in range(len(last_scores)):
+            if varvars[b] < self.threshold:
+                if self.stop_inds[b] > 0 and not self.stopped[b]:
+                    self.stopped[b] = self.stop_inds[b] >= self.size
+                else:
+                    self.stop_inds[b] = int(
+                        min(max(self.size, 1) * 1.15 + 150 + self.window_size, 4096)
+                    )
+            else:
+                self.stop_inds[b] = 0
+                self.stopped[b] = False
+        return all(self.stopped.values()) and len(self.stopped) > 0
+
+
+def batch(l, b=15):
+    subs = []
+    for i in range(len(l) - b):
+        subs.append(l[i: i+b])
+    return subs
+
+def subdiv(l, b=10):
+    subs = []
+    for i in range(len(l) - b):
+        subs.append(l[: i + b])
+    return subs
+
+class NougatModel(PreTrainedModel):
+    r"""
+    Nougat: Neural Optical UnderstandinG for Academic documents.
+    The encoder converts an image of an academic document into a series of embeddings.
+    Then, the decoder generates a sequence of tokens based on encoder's output.
+    This sequence can be translated into a structured markup language format.
+    """
+    config_class = NougatConfig
+    base_model_prefix = "nougat"
+
+    def __init__(self, config: NougatConfig):
+        super().__init__(config)
+        self.encoder = SwinEncoder(
+            input_size=self.config.input_size,
+            align_long_axis=self.config.align_long_axis,
+            window_size=self.config.window_size,
+            encoder_layer=self.config.encoder_layer,
+            name_or_path=self.config.name_or_path,
+            patch_size=self.config.patch_size,
+            embed_dim=self.config.embed_dim,
+            num_heads=self.config.num_heads,
+        )
+        self.decoder = BartDecoder(
+            max_position_embeddings=self.config.max_position_embeddings,
+            decoder_layer=self.config.decoder_layer,
+            name_or_path=self.config.name_or_path,
+            hidden_dimension=self.config.hidden_dimension,
+        )
+
+    def forward(
+            self,
+            image_tensors: torch.Tensor,
+            decoder_inputs_ids: torch.Tensor,
+            attention_mask: Optional[torch.Tensor] = None,
+    ):
+        """
+        Calculate a loss given an input image and a desired token sequence,
+        the model will be trained in a teacher-forcing manner
+
+        Args:
+            image_tensors: (batch_size, num_channels, height, width)
+            decoder_input_ids: (batch_size, sequence_length, embedding_dim)
+        """
+        encoder_outputs = self.encoder(image_tensors)
+        """
+        在PyTorch中，有些操作要求输入张量的存储方式是连续的，否则可能会导致运行时错误。
+        当对一个非连续的张量执行某些操作时，PyTorch会自动调用contiguous()来确保它是连续的。
+        然而，为了提高效率和避免不必要的内存复制，有时候在关键的地方显式调用contiguous()是一个好的实践。
+        """
+        decoder_outputs = self.decoder(
+            input_ids=decoder_inputs_ids[:, : -1].contiguous(),
+            encoder_hidden_states=encoder_outputs,
+            attention_mask=attention_mask[:, : -1],
+            labels = decoder_inputs_ids[:, 1:].contiguous(),
+        )
+
+        return decoder_outputs
+    
+    def _init_weights(self, *args, **kwargs):
+        return
+    
+    def inference(
+            self,
+            image: Image.Image = None,
+            image_tensors: Optional[torch.Tensor] = None,
+            return_attentions: bool = False,
+            early_stopping: bool = True,
+    ):
+        outputs = {
+            "predictions": list(),
+            "sequences": list(),
+            "repeats": list(),
+            "repetitions": list(),
+        }
+        if image_tensors is None:
+            image_tensors = self.encoder.prepare_input(image).unsqueeze(0)
+        if self.device.type != "mps":
+            image_tensors = image_tensors.to(next(self.parameters()).dtype)
+
+        image_tensors = image_tensors.to(self.device)
+
+        last_hidden_state = self.encoder(image_tensors)
+
+        encoder_outputs = ModelOutput(
+            last_hidden_state=last_hidden_state,
+            attention=None,
+        )
+        if len(encoder_outputs.last_hidden_state.size()) == 1:
+            encoder_outputs.last_hidden_state = (
+                encoder_outputs.last_hidden_state.unsqueeze(0)
+            )
+        
+        # get decoder output
+        decoder_outputs = self.decoder.model.generate(
+            encoder_outputs=encoder_outputs,
+            min_length=1,
+            max_length=self.config.max_length,
+            pad_token_id = self.decoder.tokenizer.pad_token_id,
+            eos_token_id = self.decoder.tokenizer.eos_token_id,
+            use_cache=True,
+            bad_words_ids=[
+                [self.decoder.tokenizer.unk_token_id],
+            ],
+            return_dict_in_generation=True,
+            output_scores=True,
+            output_attentions=return_attentions,
+            do_sample=False,
+            stopping_criteria=StoppingCriteriaList(
+                [StoppingCriteriaScores()] if early_stopping else []
+            ),      
+        )     
+        outputs["repetitions"] = decoder_outputs.sequences.clone()
+        outputs["sequences"] = decoder_outputs.sequences.clone()
+        batch_size = len(decoder_outputs.sequences)
+
+        logits = torch.stack(decoder_outputs.scores, 1).cpus().max(-1)
+        values = logits.values
+        indices = logits.indices
+
+        for b in range(batch_size):
+            mask = indices[b] != self.decoder.tokenizer.pad_token_id
+            N = mask.sum().item()
+            var = np.array(
+                [np.var(s) /len(s) for s in batch(values[b, mask].float().numpy())]
+            )
+            if len(var) < 10:
+                outputs["repeats"].append(None)
+                continue
+            varvar = np.array([np.var(v) for v in subdiv(var[::-1])][::-1])
+            minlen = 120
+            if (
+                indices[b] == self.decoder.tokenizer.eos_token_id
+            ).any()  and N + 1 < indices.shape[1]:
+            # there is an end to the generation, likely no repetitions
+                outputs["repeats"].append(None)
+                continue
+            small_var = np.where(varvar < 0.045)[0]
+            if early_stopping and len(small_var) > 1:
+                if np.all(np.diff(small_var) < 2):
+                    idx = int(min(max(small_var[0], 1) * 1.08 + minlen, 4096))
+                    if idx / N > 0.9: # at most last bit
+                        outputs["repeats"].append(None)
+                        continue
+                    elif small_var[0] < 30:
+                        idx = 0
+                    logging.warn("Found repetitions in sample %i" % b)
+                    outputs["repeats"].append(idx)
+                    outputs["sequences"][b, idx:] = self.decoder.tokenizer.pad_token_id
+                    outputs["repetitions"][b, :idx] = self.decoder.tokenizer.pad_token_id
+                else:
+                    outputs["repeats"].append(None)
+            else:
+                outputs["repeats"].append(None)
+        outputs["repitations"] = self.decoder.tokenizer.batch_decode(outputs["repetitions"],
+                                                                     skip_special_tokens=True)
+        outputs["predictions"] = self.decoder.tokenizer.batch_decode(outputs["sequences"],
+                                                                     skip_special_tokens=True)
+        outputs["predictions"] = postprocess(
+            self.decoder.tokenizer.batch_decode(
+                outputs["sequences"], skip_special_tokens=True
+            ),
+            markdown_fix=False
+        )
+
+        if return_attentions:
+            outputs["attentions"] = {
+                "self_attentions": decoder_outputs.decoder_attentions,
+                "cross_attentions": decoder_outputs.cross_attentions,
+            }
+        return outputs
+    
+    @classmethod
+    def from_pretrained(
+        cls,
+        model_path: Union[str, bytes, os.PathLike],
+        *model_args,
+        **kwargs,
+    ):
+        r"""
+        Instantiate a pretrained nougat model from a pre-trained model configuration
+
+        Args:
+            model_path:
+                Name of a pretrained model name either registered in huggingface.co. or saved in local.
+        """
+        model = super(NougatModel, cls).from_pretrained(model_path, *model_args, **kwargs)
+
+        # truncate or interpolate position embeddings of decoder
+        max_length = kwargs.get("max_length", model.config.max_position_embeddings)
+        if (
+            max_length != model.config.max_position_embeddings
+        ):# if max_length of trained model differs max_length you want to train
+            model.decoder.model.model.decoder.embed_positions.weight = torch.nn.Parameter(
+                model.decoder.model.model.decoder.embed_positions.weight,
+                max_length + 2,# https://github.com/huggingface/transformers/blob/v4.11.3/src/transformers/models/mbart/modeling_mbart.py#L118-L119
+            )
+            model.config.max_position_embeddings = max_length
+        
+        return model
 
